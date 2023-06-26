435fca696b71ef8921c99cc042964cab22124e0b
Durmuş <demirtasdurmus@gmail.com>
2023-06-26 16:38:35 +0300

    test(d-client): add test setup

---
+++ apps/d-client/src/components/Accordion/Accordion.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { Accordion } from './Accordion';
+
+describe('Accordion test', () => {
+    test('Should show title one-1', () => {
+        render(
+            <Accordion title="Testing">
+                <h4>Content</h4>
+            </Accordion>,
+        );
+
+        expect(screen.getByText(/Testing/i)).toBeDefined();
+    });
+    test('Should show title two-2', () => {
+        render(
+            <Accordion title="test">
+                <p>test</p>
+            </Accordion>,
+        );
+        const element = screen.getByRole('heading');
+        expect(element).toBeDefined();
+        expect(element).toBeInTheDocument();
+    });
+});
--- apps/d-client/vite.config.ts
+++ apps/d-client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
     plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.ts',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'html'],
+            exclude: ['node_modules/', './vitest.setup.ts'],
+        },
+    },
 });
