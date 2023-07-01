2b7c6222b7dedc1bce4adfd77c1f34fee82c1a9f
Durmuş <durmus.demirtas@anywai.ch>
2023-07-02 01:01:10 +0300

    fix(d-server): add typecheck for lint stage (#9)

--- apps/d-server/src/index.ts
+++ apps/d-server/src/index.ts
@@ -12,5 +12,5 @@ const server = createServer(app);
 
 server.listen(PORT, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server listening on port ${PORT}.`);
+    console.log(`Server listening on port ${PORT}`);
 });
